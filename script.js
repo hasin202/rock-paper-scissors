aa91224af868d0fe7b4e8007ebeebfc1073bb299
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-06 11:25:48 +0100

    render title through js

--- script.js
+++ script.js
@@ -18,16 +18,14 @@
 
 //---------------------------------------------------------------------
 
-let optionsArray = ["rock", "paper", "scissors"];
-
 const getComputerChoice = () => {
+  let optionsArray = ["rock", "paper", "scissors"];
   let randomNumber = Math.floor(Math.random() * optionsArray.length);
   return optionsArray[randomNumber];
 };
 
 const getPlayerChoice = (event) => {
   const playerChoice = event.target.closest("button");
-  console.log(playerChoice);
   if (!playerChoice) {
     return;
   }
@@ -41,42 +39,47 @@ const getPlayerChoice = (event) => {
 const playRound = (computerChoice, playerChoice) => {
   console.log(`comp choice: ${computerChoice}`);
   console.log(`plyr choice: ${playerChoice.toLowerCase()}`);
-  //   console.log(computerChoice);
-  //   console.log(playerChoice.toLowerCase());
+
   if (playerChoice.toLowerCase() === computerChoice) {
     console.log("DRAW");
   }
 };
 
-const choice = document.querySelector(".buttons");
-choice.addEventListener("click", (event) => {
-  const player_rps_option = getPlayerChoice(event);
-  if (player_rps_option === undefined) {
-    return;
-  } else {
-    playRound(getComputerChoice(), getPlayerChoice(event));
-  }
-});
-
-// function ready(callback) {
-//   // in case the document is already rendered
-//   if (document.readyState != "loading") callback();
-//   else if (document.addEventListener)
-//     document.addEventListener("DOMContentLoaded", callback);
-//   else
-//     document.attachEvent("onreadystatechange", function () {
-//       if (document.readyState == "complete") callback();
-//     });
-// }
-
-// ready(function () {
-//   const choice = document.querySelector(".buttons");
-//   choice.addEventListener("click", (event) => {
-//     const player_rps_option = getPlayerChoice(event);
-//     if (player_rps_option === undefined) {
-//       return;
-//     } else {
-//       playRound(getComputerChoice(), getPlayerChoice(event));
-//     }
-//   });
+// const choice = document.querySelector(".buttons");
+// choice.addEventListener("click", (event) => {
+//   const player_rps_option = getPlayerChoice(event);
+//   if (player_rps_option === undefined) {
+//     return;
+//   } else {
+//     playRound(getComputerChoice(), getPlayerChoice(event));
+//   }
 // });
+
+function ready(callback) {
+  // in case the document is already rendered
+  if (document.readyState != "loading") callback();
+  else if (document.addEventListener)
+    document.addEventListener("DOMContentLoaded", callback);
+  else
+    document.attachEvent("onreadystatechange", function () {
+      if (document.readyState == "complete") callback();
+    });
+}
+
+ready(function () {
+  let title = document.createElement("div");
+
+  title.className = " font-bold text-white text-5xl";
+  title.textContent = "ROCK PAPER SCISSORS";
+
+  document.getElementById("text-output-container").appendChild(title);
+  const choice = document.querySelector(".buttons");
+  choice.addEventListener("click", (event) => {
+    const player_rps_option = getPlayerChoice(event);
+    if (player_rps_option === undefined) {
+      return;
+    } else {
+      playRound(getComputerChoice(), getPlayerChoice(event));
+    }
+  });
+});
