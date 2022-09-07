8c9e26d3d7adb328101dd2c745bf14439619a642
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-07 12:31:17 +0100

    refactored some code and hard coded title

--- script.js
+++ script.js
@@ -1,53 +1,31 @@
-// // console.log(getComputerChoice());
-
-// function ready(callback) {
-//   // in case the document is already rendered
-//   if (document.readyState != "loading") callback();
-//   else if (document.addEventListener)
-//     document.addEventListener("DOMContentLoaded", callback);
-//   else
-//     document.attachEvent("onreadystatechange", function () {
-//       if (document.readyState == "complete") callback();
-//     });
-// }
-
-// const choice = document.querySelector(".buttons");
-// choice.addEventListener("click", (event) => {
-//   const player_rps_option = getPlayerChoice(event);
-//   if (player_rps_option === undefined) {
-//     return;
-//   } else {
-//     playRound(getComputerChoice(), getPlayerChoice(event));
-//   }
-// });
-
-// ready(function () {
-//   console.log(getPlayerChoice());
-//   //   playRound(getComputerChoice, getPlayerChoice);
-// });
-
 //---------------------------------------------------------------------
 let playerScore = 0;
 let compScore = 0;
 let gameMessage = "";
 let compChoiceText;
 
+const displayCompChoice = (compChoice) => {
+  if (compChoiceText === undefined) {
+    compChoiceText = document.createElement("div");
+    compChoiceText.className =
+      "flex justify-center items-center px-4 py-2font-light text-white text-4xl border border-white rounded";
+  }
+
+  compChoiceText.innerHTML = `<p>
+      Computer chose: ${compChoice}
+    </p>`;
+  document.getElementById("text-output-container").appendChild(compChoiceText);
+};
+
 const getComputerChoice = () => {
   let optionsArray = ["rock", "paper", "scissors"];
   let randomNumber = Math.floor(Math.random() * optionsArray.length);
   console.log(compChoiceText);
+  const compChoice = optionsArray[randomNumber];
 
-  if (compChoiceText === undefined) {
-    compChoiceText = document.createElement("div");
-    compChoiceText.className = " font-light text-white text-4xl";
-  }
+  displayCompChoice(compChoice);
 
-  compChoiceText.innerHTML = `<p>
-      Computer chose: ${optionsArray[randomNumber]}
-    </p>`;
-  document.getElementById("text-output-container").appendChild(compChoiceText);
-
-  return optionsArray[randomNumber];
+  return compChoice;
 };
 
 const getPlayerChoice = (event) => {
@@ -102,11 +80,11 @@ function ready(callback) {
 ready(function () {
   console.log("---------------------");
 
-  let title = document.createElement("div");
-  title.className = " font-bold text-white text-5xl";
+  // let title = document.createElement("div");
+  // title.className = " font-bold text-white text-5xl";
 
-  title.textContent = "ROCK PAPER SCISSORS";
-  document.getElementById("text-output-container").appendChild(title);
+  // title.textContent = "ROCK PAPER SCISSORS";
+  // document.getElementById("text-output-container").appendChild(title);
 
   const choice = document.querySelector(".buttons");
   choice.addEventListener("click", (event) => {
